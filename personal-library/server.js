1fbd9d8006d832882b944676cd6ada8c08f814e4
Aizistral <admin@aizistral.com>
2023-12-01 19:45:15 +0100

    Ensure personal library tests run after DB connection

--- personal-library/server.js
+++ personal-library/server.js
@@ -35,24 +35,25 @@ apiRoutes(app).then(() => {
             .type('text')
             .send('Not Found');
     });
+
+    //Start our server and tests!
+    const listener = app.listen(process.env.PORT || 3000, async function () {
+        console.log('Your app is listening on port ' + listener.address().port);
+
+        if (process.env.NODE_ENV === 'test') {
+            console.log('Running Tests...');
+            setTimeout(function () {
+                try {
+                    runner.run();
+                } catch (e) {
+                    console.log('Tests are not valid:');
+                    console.error(e);
+                }
+            }, 1500);
+        }
+    });
 }).catch(err => {
     throw err;
-});
-
-//Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-    console.log('Your app is listening on port ' + listener.address().port);
-    if (process.env.NODE_ENV === 'test') {
-        console.log('Running Tests...');
-        setTimeout(function () {
-            try {
-                runner.run();
-            } catch (e) {
-                console.log('Tests are not valid:');
-                console.error(e);
-            }
-        }, 1500);
-    }
 });
 
 module.exports = app; //for unit/functional testing
